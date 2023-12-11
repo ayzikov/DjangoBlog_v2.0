da65c0e3a8199132c5d0c4d08ae7a56958b5d13f
RevelRies <ayzikov070@mail.ru>
2023-12-11 20:12:08 +0300

    feat: add automatic filling for the post slug field

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
 from .models import Post, Category
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 
 @admin.register(Category)
 class CategoryAdmin(DjangoMpttAdmin):
