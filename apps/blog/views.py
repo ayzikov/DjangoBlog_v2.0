e21446a636a2f5e51d93c0ed136d345c14d48432
RevelRies <ayzikov070@mail.ru>
2023-12-11 20:49:52 +0300

    feat: add context for PostListView

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -8,3 +8,8 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['title'] = 'Главная страница'
+        return context
