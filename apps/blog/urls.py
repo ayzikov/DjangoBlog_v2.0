ef47e339a11e33864324e40c7f1ffee0f97487f8
RevelRies <ayzikov070@mail.ru>
2023-12-11 20:08:16 +0300

    fix: change view import

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,4 +1,4 @@
-import views
+from .views import PostListView
 
 
 from django.urls import path
@@ -6,7 +6,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='home'),
+    path('', PostListView.as_view(), name='home'),
 ]
 
 
